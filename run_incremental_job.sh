f4ca78a14ee7df9c34d64a19b71d0e9c1c802026
王瑞 <wangrui@uniondrug.com>
2022-01-26 14:22:38 +0800

    fix

--- run_incremental_job.sh
+++ run_incremental_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /opt/flink-1.13.2/bin
-nohup ./flink run -t yarn-per-job -d -Dyarn.application.queue=root.users.flink -Dyarn.application.name=incr_gc_source_sku_zipper_hudi_job -c cn.wr.zipper.table.datastream.IncrGoodsSkuZipperTableHudiJob /home/wr/gc_source_sku_zipper_hudi_jar/zipper-table-hudi-1.0-SNAPSHOT.jar --conf /home/wr/gc_source_sku_zipper_hudi_jar/application.properties
+nohup ./flink run -t yarn-per-job -d  -Dyarn.application.name=incr_gc_source_sku_zipper_hudi_job -c cn.wr.zipper.table.datastream.IncrGoodsSkuZipperTableHudiJob /home/wr/gc_source_sku_zipper_hudi_jar/zipper-table-hudi-1.0-SNAPSHOT.jar --conf /home/wr/gc_source_sku_zipper_hudi_jar/application.properties
--- run_initial_job.sh
+++ run_initial_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /opt/flink-1.13.2/bin
-nohup ./flink run -t yarn-per-job -d -Dyarn.application.queue=root.users.flink -Dyarn.application.name=inital_gc_source_sku_zipper_hudi_job -c cn.wr.zipper.table.datastream.InitialGoodsSkuZipperTableHudiJob /home/wr/gc_source_sku_zipper_hudi_jar/uniondrug-zipper-table-hudi-1.0-SNAPSHOT.jar --conf /home/wr/gc_source_sku_zipper_hudi_jar/application.properties
+nohup ./flink run -t yarn-per-job -d -Dyarn.application.queue=root.users.admin -Dyarn.application.name=inital_gc_source_sku_zipper_hudi_job -c cn.wr.zipper.table.datastream.InitialGoodsSkuZipperTableHudiJob /home/wr/gc_source_sku_zipper_hudi_jar/uniondrug-zipper-table-hudi-1.0-SNAPSHOT.jar --conf /home/wr/gc_source_sku_zipper_hudi_jar/application.properties
