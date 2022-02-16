05e35876ed7ea66c4c80e62f5c29e9f6df0af01b
王瑞 <jackwang9312@163.com>
2022-02-16 17:37:06 +0800

    增量接入数据开发

--- generic-zipper-table/src/main/java/cn/wr/datastream/IncrZipperTableHudiJob.java
+++ generic-zipper-table/src/main/java/cn/wr/datastream/IncrZipperTableHudiJob.java
@@ -2,15 +2,19 @@ package cn.wr.datastream;
 
 import cn.wr.constants.PropertiesConstants;
 import cn.wr.flatmap.CanalTransModelFlatMap;
+import cn.wr.flatmap.UpdateOrInsertDataFlatMap;
 import cn.wr.model.CanalTransDataModel;
 import cn.wr.utils.ExecutionEnvUtil;
 import cn.wr.utils.KafkaConfigUtil;
+import cn.wr.utils.ParseDdlUtil;
+import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,5 +37,12 @@ public class IncrZipperTableHudiJob {
         DataStreamSource<String> data = KafkaConfigUtil.createZipperTableSource(env)
                 .setParallelism(parameterTool.getInt(PropertiesConstants.STREAM_SOURCE_PARALLELISM, 1));
         DataStream<CanalTransDataModel> canalMode = data.flatMap(new CanalTransModelFlatMap());
+        RowTypeInfo rowTypeInfo = ParseDdlUtil.getRowTypeInfo(parameterTool);
+        String tableName = ParseDdlUtil.getTableName(parameterTool,0);
+        DataStream<Row> goodsSku = canalMode.flatMap(new UpdateOrInsertDataFlatMap()).returns(rowTypeInfo);
+        ste.createTemporaryView(tableName, goodsSku);
+//        ste.executeSql(parameterTool.get(SqlTypeEnum.getRealValue(3)));
+//        ste.executeSql(parameterTool.get(SqlTypeEnum.getRealValue(4)));
+        env.execute("increment_upsert_job");
     }
 }
